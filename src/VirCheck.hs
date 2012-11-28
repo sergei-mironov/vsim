cc1f90b8cf4d75285c186fdcb82e5e6c5d94be90
Sergey Mironov <ierton@gmail.com>
2012-11-28 16:43:32 +0400

    VirCheck: cut off all locations

--- src/VirCheck.hs
+++ src/VirCheck.hs
@@ -1,15 +1,22 @@
 module Main where
 
+import Data.Generics
 import System.IO
 import System.Exit
 import System.Environment
 import Text.Show.Pretty as PP
 
+import VSim.Data.Loc
+import VSim.Data.Line
 import VSim.VIR
+
+-- | Clean all the location and line info
+noloc :: IRTop -> IRTop
+noloc = everywhere (mkT (const NoLoc)) . everywhere (mkT (const LineUnknown))
 
 main = do
     hSetBuffering stdout NoBuffering
     files <- getArgs
     ps <- parseFiles files
-    putStrLn $ PP.ppShow ps
+    putStrLn $ PP.ppShow $ map noloc ps
 
