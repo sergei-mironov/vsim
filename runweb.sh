aea138a23c37b215f5d8b014c6ac2e4345f3340c
Sergey Mironov <ierton@gmail.com>
2013-02-08 00:04:08 +0400

    web: fix Linux webserver name

--- runweb.sh
+++ runweb.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-REPO=/home/Mironov.s/proj/vsim
+REPO=$HOME/proj/vsim
 
 . $REPO/simenv >/dev/null || {
 		echo "Failed to include simenv; Please fix runweb.sh"
 		exit 1
 	}
 
-(cd $REPO && ghc --make happstack/WebServer.hs ; ) &&
+(cd $REPO && ghc --make happstack/WebServer.hs -o happstack/WebServer.exe; ) &&
 echo running >&2 &&
 $REPO/happstack/WebServer.exe `ospath $REPO`
 
